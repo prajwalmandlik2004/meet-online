c99b12d621d209a1daf9228c35c81dc0d9e19092
Prajwal Mandlik <prajwalmandlik2004@gmail.com>
2024-04-02 21:22:41 +0530

    first commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from '@clerk/nextjs';
+
+export default function SiginInPage() {
+    return (
+        <main className="flex h-screen w-full items-center justify-center">
+            <SignIn />
+        </main>
+    );
+}
+
